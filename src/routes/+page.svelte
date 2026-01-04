1f031bbd2f8f25767c320e682442a93ebc161b60
Wahhab Baldwin <wahhabb@gmail.com>
2026-01-04 09:37:44 -0800

    Add link to blog page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h2>See my <a href="/blog/post1">First blog post</a></h2>
