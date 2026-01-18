7c2d7e718da15e952a2c4d195add58326c1ed241
Wahhab Baldwin <wahhabb@gmail.com>
2026-01-17 22:19:29 -0800

    Add Header, etc.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+</header>
+
+<style>
+	:global(body) {
+		margin: 0 2em;	
+	}
+	header {
+	  	display: flex;
+  		background-color: DodgerBlue;
+		padding: 1em;
+        margin: 0 -2em;
+		a {
+			background-color: darkblue;
+			font-size: 1.3em;
+			color: white;
+			padding: 1em 3em;
+			margin: 10px;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/Header.svelte';
 
 	let { children } = $props();
 </script>
-
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header />
 {@render children()}
